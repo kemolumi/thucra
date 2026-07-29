00ce4e02a4b35291c42cc0022bf17722a704a491
kemolumi <31346263+kemolumi@users.noreply.github.com>
2026-07-29 12:45:49 +0700

    Use the standard linux application directory

--- src/lib.rs
+++ src/lib.rs
@@ -3,56 +3,60 @@ pub mod vnpt;
 pub mod app;
 
 use std::path::Path;
-use tokio::{ fs::create_dir, process };
 use tracing_appender::non_blocking::WorkerGuard;
 use tracing_subscriber::{ layer::SubscriberExt, util::SubscriberInitExt };
 
 /// Must be called first for any interaction with the router.
-pub async fn init() -> Result<Option<WorkerGuard>, ()> {
-    let mut guard = None;
-    match std::env::home_dir() {
-        Some(home) => {
-            let file_appender = tracing_appender::rolling::daily(
-                format!("{}/.local/share/thucra/logs", home.as_os_str().to_str().unwrap()),
-                "thucra.log"
-            );
-            let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
-            guard = Some(_guard);
+pub fn init() -> Result<WorkerGuard, ()> {
+    let Some(home) = std::env::home_dir() else {
+        tracing::error!("No user home folder found.");
+        return Err(());
+    };
 
-            tracing_subscriber::Registry
-                ::default()
-                .with(tracing_subscriber::fmt::layer().with_ansi(false).with_writer(non_blocking))
-                .with(tracing_subscriber::fmt::layer())
-                .init();
-        }
-        None => {
-            eprintln!("No use home folder found, fallback to console logging.");
-            tracing_subscriber::fmt::init();
-        }
-    }
+    let app_folder = format!(
+        "{}/.local/share/com.kemolumi.thucra",
+        home.as_os_str().to_str().unwrap()
+    );
+
+    let file_appender = tracing_appender::rolling::daily(
+        format!("{app_folder}/logs"),
+        "thucra.log"
+    );
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+
+    tracing_subscriber::Registry
+        ::default()
+        .with(tracing_subscriber::fmt::layer().with_ansi(false).with_writer(non_blocking))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
 
     rustls::crypto::ring::default_provider().install_default().unwrap();
 
     tracing::info!("Checking SSL store...");
 
-    match Path::new("store/key.pem").exists() && Path::new("store/cert.pem").exists() {
+    let (key_location, cert_location) = (
+        format!("{app_folder}/certs/key.pem"),
+        format!("{app_folder}/certs/cert.pem"),
+    );
+
+    match Path::new(&key_location).exists() && Path::new(&cert_location).exists() {
         true => {
-            tracing::info!("SSL found. Launching server...");
-            return Ok(guard);
+            tracing::info!("SSL found.");
+            return Ok(_guard);
         }
         false => {
             tracing::warn!("No SSL store found, generating new ones...");
         }
     }
 
-    match create_dir("store").await {
+    match std::fs::create_dir(format!("{app_folder}/certs")) {
         Ok(_) => {}
         Err(error) => {
             tracing::error!("Can't create directory: {error}");
         }
     }
 
-    let status = process::Command
+    let status = std::process::Command
         ::new("openssl")
         .args([
             "req",
@@ -64,15 +68,15 @@ pub async fn init() -> Result<Option<WorkerGuard>, ()> {
             "3650",
             "-nodes",
             "-keyout",
-            "store/key.pem",
+            &key_location,
             "-out",
-            "store/cert.pem",
+            &cert_location,
             "-subj",
             "/CN=localhost",
             "-addext",
             "subjectAltName=DNS:localhost,IP:127.0.0.1",
         ])
-        .output().await;
+        .output();
 
     let success = match status {
         Ok(status) => status.status.success(),
@@ -94,5 +98,5 @@ pub async fn init() -> Result<Option<WorkerGuard>, ()> {
         }
     }
 
-    return Ok(guard);
+    return Ok(_guard);
 }
